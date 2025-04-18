cb3e890fd21fbcab537fe83917792f41e9bd07b4
Wsalas651 <ws2022073896@virtual.upt.pe>
2025-04-18 17:57:09 -0500

    Fixing the QUALITY GATE STATUS

--- Bank/Bank.WebApi/Models/BankAccount.cs
+++ Bank/Bank.WebApi/Models/BankAccount.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics.CodeAnalysis; // <- Asegúrate de incluir este using
-
 namespace Bank.WebApi.Models
 {
     public class BankAccount
@@ -7,15 +5,8 @@ namespace Bank.WebApi.Models
         private readonly string m_customerName;
         private double m_balance;
 
-        // Constructor privado requerido por algunos frameworks
-        [ExcludeFromCodeCoverage] // <- Esto excluye este constructor del análisis de cobertura
-        private BankAccount()
-        {
-            m_customerName = string.Empty; // Evita advertencia CS8618
-            m_balance = 0;
-        }
+        private BankAccount() { }
 
-        // Constructor principal con parámetros
         public BankAccount(string customerName, double balance)
         {
             m_customerName = customerName ?? throw new ArgumentNullException(nameof(customerName));
@@ -23,21 +14,22 @@ namespace Bank.WebApi.Models
         }
 
         public string CustomerName => m_customerName;
-
         public double Balance => m_balance;
 
         public void Debit(double amount)
         {
             ArgumentOutOfRangeException.ThrowIfGreaterThan(amount, m_balance);
             ArgumentOutOfRangeException.ThrowIfNegative(amount);
+
             m_balance -= amount;
         }
 
         public void Credit(double amount)
         {
             ArgumentOutOfRangeException.ThrowIfNegative(amount);
+
             m_balance += amount;
         }
+
     }
 }
-
